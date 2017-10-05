78911229b8475d5af190ca2e0aa2c7f6562fa634
Decorus2009 <Decorus2009@mail.ru>
2017-10-05 23:51:45 +0300

    added Spring Boot file uploader

---
+++ Spring Boot File Upload/src/main/java/com/dropzone/controller/UploadController.java
@@ -0,0 +1,59 @@
+package com.dropzone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class UploadController {
+    //Save the uploaded file to this folder
+    private static String UPLOAD_FOLDER =
+            "." + File.separator + "src" + File.separator + "main" + File.separator +
+                    "resources" + File.separator + "uploaded" + File.separator;
+
+
+    public UploadController() {
+        if (!Files.exists(Paths.get(UPLOAD_FOLDER))) {
+            new File(UPLOAD_FOLDER).mkdir();
+        }
+    }
+
+    @GetMapping({"/", "/upload"})
+    public String index() {
+        return "upload";
+    }
+
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+}
