fb5ff6236e44859d6420e3a13370ad2c7c8e7646
Decorus2009 <Decorus2009@mail.ru>
2017-12-07 02:13:28 +0300

    fixed path for add.pid

--- DropZone/src/main/java/dropzone/Application.java
+++ DropZone/src/main/java/dropzone/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
-        application.addListeners(new ApplicationPidFileWriter("./run_script/app.pid"));
+        application.addListeners(new ApplicationPidFileWriter("app.pid"));
         application.run();
     }
 }
